3c555c38482d0f64125605232910eedb1890aeb7
Jamesart05 <130219176+Jamesart05@users.noreply.github.com>
2025-07-29 20:29:04 +0100

    trying once more

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -45,29 +45,29 @@ const About  = () => {
                 </div>
             </section>
             <h2 className="text-center text-[2.6rem] font-bold p-20 pt-40 pb-20">Testimonials</h2>
-            <main className="flex flex-col gap-56 items-center pb-20">
-                <div className="lg:w-[64%] px-12 lg:px-0 flex flex-wrap justify-center gap-12">
-                    <img className="w-[16rem] h-[16rem]" src={img5} alt="" />
-                    <div className="flex flex-col justify-between">
+            <main className="flex flex-col gap-24 lg:gap-48 items-center pb-20 ">
+                <div className="lg:w-[64%] px-8 lg:px-0 flex flex-wrap justify-center lg:gap-12">
+                    <img className="w-[26%] h-[16rem]" src={img5} alt="" />
+                    <div className="w-[64%] flex flex-col justify-between">
                         <img className="w-fit" src={img8}alt="" />
-                        <p className="text-[1.4rem] font-bold text-gray-700 py-4">Once we ordered some accessories items and we got the <br /> products delivered in our doorstep, the customer support <br /> was super helpful and they answered all my queries.</p>
-                        <p className="text-[1.4rem] font-medium">Stacy</p>
+                        <p className="lg:text-[1.4rem] font-bold text-gray-700 py-4">Once we ordered some accessories items and we got the <br /> products delivered in our doorstep, the customer support <br /> was super helpful and they answered all my queries.</p>
+                        <p className="lg:text-[1.4rem] font-medium">Stacy</p>
                     </div>
                 </div>
-                <div className="lg:w-[64%] px-12 lg:px-0 flex flex-wrap justify-center gap-12">
-                    <img className="w-[16rem] h-[16rem]" src={img6} alt="" />
-                    <div className="flex flex-col justify-between">
+                <div className="lg:w-[64%] px-8 lg:px-0 flex flex-wrap justify-center lg:gap-12">
+                    <img className="w-[26%] h-[16rem]" src={img6} alt="" />
+                    <div className="w-[64%] flex flex-col justify-between">
                         <img className="w-fit" src={img8} alt="" />
-                        <p className="text-[1.4rem] font-bold text-gray-700 py-4">I ordered 5 shirts from them and received them in no time. <br /> The customer support was awesome.</p>
-                        <p className="text-[1.4rem] font-medium">Tiffany</p>
+                        <p className="lg:text-[1.4rem] font-bold text-gray-700 py-4">I ordered 5 shirts from them and received them in no time. <br /> The customer support was awesome.</p>
+                        <p className="lg:text-[1.4rem] font-medium">Tiffany</p>
                     </div>
                 </div>
-                <div className="lg:w-[64%] px-12 lg:px-0 flex flex-wrap justify-center gap-12">
-                    <img className="w-[16rem] h-[16rem]" src={img7} alt="" />
-                    <div className="flex flex-col justify-between">
+                <div className="lg:w-[64%] px-8 lg:px-0 flex flex-wrap justify-center lg:gap-12">
+                    <img className="w-[26%] h-[16rem]" src={img7} alt="" />
+                    <div className="w-[64%] flex flex-col justify-between">
                         <img className="w-fit" src={img8} alt="" />
-                        <p className="text-[1.4rem] font-bold text-gray-700 py-4">I got a wrong shirt so I contacted them and they happily offered me a refund. I will surely shop from them again.</p>
-                        <p className="text-[1.4rem] font-medium">James</p>
+                        <p className="lg:text-[1.4rem] font-bold text-gray-700 py-4">I got a wrong shirt so I contacted them and they happily offered me a refund. I will surely shop from them again.</p>
+                        <p className="lg:text-[1.4rem] font-medium">James</p>
                     </div>
                 </div>
             </main>
